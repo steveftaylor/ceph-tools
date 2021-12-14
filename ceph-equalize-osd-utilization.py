75922c866c6513c5403d3d5082b76cf43e534a9d
Steve Taylor <steveftaylor@gmail.com>
2021-12-14 13:37:13 -0700

    Updates for Octopus

--- ceph-equalize-osd-utilization.py
+++ ceph-equalize-osd-utilization.py
@@ -22,7 +22,8 @@ import time
 
 # Change these to command-line arguments
 cluster = 'ceph'
-max_reweight_attempts = 1000
+num_reweight_osds_per_iteration = 1
+max_reweight_attempts = 1000000
 devnull = open(os.devnull, 'w')
 
 
@@ -70,7 +71,7 @@ for id in osd_disk_size:
 print('Getting pool information')
 pool_dividend = {int(fields[0]): int(fields[2]) if fields[1] == 'erasure' else 1 for fields in [x.split() for x in
                                                                                                 command_output(
-                                                                                                    """osdmaptool osdmap --dump | grep pool | awk '{{print $2 " " $4 " " $8}}'""").split(
+                                                                                                    """osdmaptool osdmap --dump | grep pool | awk '{{print $2 " " $4 " " $10}}'""").split(
                                                                                                     '\n') if x != '']}
 
 print('Getting current OSD crush weights')
@@ -87,7 +88,7 @@ pg_size = {fields[0]: int(fields[1]) for fields in [x.split() for x in command_o
 def get_osd_variance():
     osd_size = {osd: 0 for osd in osds}
     pg_data = [x for x in command_output(
-        """osdmaptool osdmap --test-map-pgs-dump | awk '/\\..*\\[.*\\]/{{print $1 " " $2}}'""").split('\n') if
+        """osdmaptool osdmap --test-map-pgs-dump | awk '/./ && /\[/ && /\]/{{print $1 " " $2}}'""").split('\n') if
                x != '']
     pgs = []
     pg_osds = {}
@@ -99,10 +100,14 @@ def get_osd_variance():
         pg_osds[pg_fields[0]] = ast.literal_eval(pg_fields[1])
 
     # Use the pg sizes and mappings to compute used space for each osd
+#    num_pgs = 0
     for pg in pgs:
         pool = int(pg.split('.')[0])
         for osd in pg_osds[pg]:
             osd_size[osd] += pg_size[pg] / pool_dividend[pool]
+#            if osd == 5:
+#                num_pgs += 1
+#                print('{} {} {}'.format(num_pgs, pg, osd_size[osd]))
 
     # Compute the full percentage for each OSD, compute an average, then compute each OSD's variance and return a dictionary
     osd_percent_full = {osd: float(osd_size[osd]) / osd_disk_size[osd] for osd in osds}
@@ -117,23 +122,29 @@ best_variance = max([abs(1 - variance) for variance in osd_variance.values()])
 num_reweight_attempts = 0
 
 while num_reweight_attempts < max_reweight_attempts:
-    osd = \
-    max({osd: abs(1 - variance) for osd, variance in osd_variance.items()}.iteritems(), key=operator.itemgetter(1))[0]
-    variance = osd_variance[osd]
-    increment = crush_weight[osd] * abs(1 - variance) / 100
+    osd_variances = {osd: abs(1 - variance) for osd, variance in osd_variance.items()}
+    i = 0
+    while i < num_reweight_osds_per_iteration:
+        osd = max(osd_variances.iteritems(), key=operator.itemgetter(1))[0]
+        osd_variances.pop(osd, None)
+        variance = osd_variance[osd]
+        increment = crush_weight[osd] * abs(1 - variance) / 100
 
-    if osd_variance[osd] > 1.0:
-        crush_weight[osd] -= increment
-    else:
-        crush_weight[osd] += increment
+        if osd_variance[osd] > 1.0:
+            crush_weight[osd] -= increment
+        else:
+            crush_weight[osd] += increment
 
-    if ((max_reweight_attempts - num_reweight_attempts - 1) % 10 == 0):
-        print('osd.{} has a variance of {}, reweighting to {}, {} iterations until exit'.format(osd, variance,
-                                                                                                crush_weight[osd],
-                                                                                                max_reweight_attempts - num_reweight_attempts - 1))
-    run_command(
-        'crushtool -i crushmap --reweight-item osd.{} {} -o crushmap'.format(osd, crush_weight[osd]))
-    run_command('osdmaptool osdmap --import-crush crushmap')
+        if ((max_reweight_attempts - num_reweight_attempts - 1) % 10 == 0):
+            print('osd.{} has a variance of {}, reweighting to {}, {} iterations until exit'.format(osd, variance,
+                                                                                                    crush_weight[osd],
+                                                                                                    max_reweight_attempts - num_reweight_attempts - 1))
+        run_command(
+            'crushtool -i crushmap --reweight-item osd.{} {} -o crushmap'.format(osd, crush_weight[osd]))
+        run_command('osdmaptool osdmap --import-crush crushmap')
+
+        i += 1
+
     osd_variance = get_osd_variance()
     max_variance = max([abs(1 - variance) for variance in osd_variance.values()])
 
@@ -154,6 +165,5 @@ print('Cleaning up temporary files')
 os.remove('crushmap.best')
 os.remove('crushmap')
 os.remove('osdmap')
-
 devnull.close()
 print('Done')
